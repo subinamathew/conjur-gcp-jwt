6768dc43e95634d6c2223b4e708688540211d912
subinamathew <smathew2237@gmail.com>
2023-01-06 17:10:16 +0000

    inizt

--- src/test/java/org/services/GreetingResourceIT.java
+++ src/test/java/org/services/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.services;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
--- src/test/java/org/services/GreetingResourceTest.java
+++ src/test/java/org/services/GreetingResourceTest.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.services;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
